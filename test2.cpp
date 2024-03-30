16e52a5a73a6c1e6d061fde682f0048468bd2d96
undefined2001 <theundefined2001@gmail.com>
2024-03-30 22:56:08 +0600

    sample code added for function pointer

--- test2.cpp
+++ test2.cpp
@@ -3,14 +3,5 @@
 
 int main()
 {
-    uint8_t name[] = {0x41, 0x72, 0x69, 0x66, 0x61};
-    uint8_t *name_ptr = name;
-    uint8_t size = sizeof(name) / sizeof(name[0]);
-
-    while (size--)
-    {
-        std::cout << (char)*name_ptr++;
-    }
-    std::cout << std::endl;
     return 0;
 }
