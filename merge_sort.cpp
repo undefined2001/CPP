f15182167b093c56c9473993ef9fb316b1f8229f
undefined2001 <asraful.islam.taj@gmail.com>
2025-09-14 01:49:01 +0600

    Comments added

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -3,13 +3,23 @@
 
 void merge(std::vector<int> &vec, int left, int mid, int right)
 {
+    /**
+     * *Creating Two Subarray
+     */
     std::vector<int> lvec(vec.begin() + left, vec.begin() + mid + 1);
     std::vector<int> rvec(vec.begin() + mid + 1, vec.begin() + right + 1);
 
+    /**
+     * * Adding Sentinel Value
+     */
     lvec.push_back(INT_MAX);
     rvec.push_back(INT_MAX);
 
     int i{0}, j{0}, k{left};
+
+    /**
+     * * Sorting and Storing Value to the given Array
+     */
     while (k <= right)
     {
         if (lvec[i] <= rvec[j])
@@ -25,13 +35,17 @@ void merge(std::vector<int> &vec, int left, int mid, int right)
 
 void merge_sort(std::vector<int> &vec, int start, int end)
 {
+    //* Base Case Scenario
     if (start >= end)
         return;
 
-    int mid = start + (end - start) / 2;
-    merge_sort(vec, start, mid);
-    merge_sort(vec, mid + 1, end);
-    merge(vec, start, mid, end);
+    int mid = start + (end - start) / 2; //* Calculating mid and this prevents the integer overflow issue
+
+    merge_sort(vec, start, mid); //* Sorting the left half
+
+    merge_sort(vec, mid + 1, end); //* Sorting the right half
+
+    merge(vec, start, mid, end); //* merging the two halves
 }
 
 void print_vector(std::vector<int> &vec)
